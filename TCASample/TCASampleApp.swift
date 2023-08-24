5f0ed94cda803163b1f915049017f93e8ee58921
Rin-t <rin.t.4921@gmail.com>
2023-08-24 16:48:44 +0900

    Initial Commit

---
+++ TCASample/TCASampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCASampleApp.swift
+//  TCASample
+//
+//  Created by Rin on 2023/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TCASampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
